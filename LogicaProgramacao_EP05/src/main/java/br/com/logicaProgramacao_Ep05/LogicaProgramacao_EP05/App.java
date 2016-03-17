aa97d2d54d3a0e1ef0c45d833cd9095b78275b88
LeonardoAndriotti <leonardoandriotte@hotmail.com>
2016-03-17 15:38:27 -0300

    update leo

---
+++ LogicaProgramacao_EP05/src/main/java/br/com/logicaProgramacao_Ep05/LogicaProgramacao_EP05/App.java
@@ -0,0 +1,13 @@
+package br.com.logicaProgramacao_Ep05.LogicaProgramacao_EP05;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
