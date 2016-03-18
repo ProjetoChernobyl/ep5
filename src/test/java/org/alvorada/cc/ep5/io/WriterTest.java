be33808f49241344853a42da25838874c0bdd216
Ricardo Luiz Silveira <ricardo.silveira@gmail.com>
2016-03-18 15:00:02 -0300

    Update WriterTest.java

--- src/test/java/org/alvorada/cc/ep5/io/WriterTest.java
+++ src/test/java/org/alvorada/cc/ep5/io/WriterTest.java
@@ -1,14 +1,25 @@
 package org.alvorada.cc.ep5.io;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
+
 
 public class WriterTest {
 
+	Writer classUnderTest = new Writer();
+
 	@Test
-	public void testWriteFile() {
-		fail("Not yet implemented");
+	public void writerFileLineByLineTest() {
+		String filePath = "src/test/java/org/alvorada/cc/ep5/files/outputtestfile";
+		
+		String expected = "testando escrita de arquivo 1515";
+
+		classUnderTest.writeFile(filePath, expected);
+		
+		Reader reader = new Reader();
+		String actual = reader.readFile(filePath);
+		
+		Assert.assertEquals(expected, actual);
 	}
 
 }
