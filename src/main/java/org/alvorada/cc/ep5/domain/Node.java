06206f7efa2ceae9dfdbc54ad68053e80b223a49
Ricardo Luiz Silveira <ricardo.silveira@gmail.com>
2016-03-18 15:02:14 -0300

    Update Node.java

--- src/main/java/org/alvorada/cc/ep5/domain/Node.java
+++ src/main/java/org/alvorada/cc/ep5/domain/Node.java
@@ -1,5 +1,36 @@
 package org.alvorada.cc.ep5.domain;
 
 public class Node {
-	// TODO criar os atributos e métodos da classe
+	
+	public static int id  = 0;
+	public char value;
+	private Node next;
+	
+	@SuppressWarnings("static-access")
+	public Node(char value){
+		this.value = value;
+		this.next = null;
+		this.id++;
+	}
+
+	public char getValue() {
+		return value;
+	}
+
+	public void setValue(char value) {
+		this.value = value;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+
+	public static int getId() {
+		return id;
+	}
+	
 }
