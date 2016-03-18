a74d6e91bdd5c10c69d49fac4900cf841e308e44
Ricardo Luiz Silveira <ricardo.silveira@gmail.com>
2016-03-18 11:12:20 -0300

    Teste da classe tools
    Teste base para o método que converte uma string em um array de inteiros

--- src/test/java/org/alvorada/cc/ep5/util/ToolsTest.java
+++ src/test/java/org/alvorada/cc/ep5/util/ToolsTest.java
@@ -1,14 +1,19 @@
 package org.alvorada.cc.ep5.util;
 
-import static org.junit.Assert.*;
+import org.junit.Assert;
 
-import org.junit.Test;
 
 public class ToolsTest {
+	
+	Tools classUnderTest = new Tools();
 
 	@Test
 	public void testStringToIntArray() {
-		fail("Not yet implemented");
+		String group = "1 2 30 32 102 152";
+		
+		int[] actuals = classUnderTest.stringToIntArray(group);
+		int[] expecteds = {1, 2, 30, 32, 102, 152};
+		Assert.assertArrayEquals(expecteds, actuals);
 	}
 
 }
