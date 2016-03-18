2b7ac70bf5ce7185aef72a33797c0670a5ce2282
LeonardoAndriotti <leonardoandriotte@hotmail.com>
2016-03-18 10:33:37 -0300

    atualização léo

--- src/main/java/org/alvorada/cc/ep5/Main.java
+++ src/main/java/org/alvorada/cc/ep5/Main.java
@@ -22,7 +22,8 @@ public class Main {
 	public static void main( String[] args ){
     	
     	Reader reader = new Reader();
-    	String a = reader.readFile(PATH_GENOMA,PATH_COOR);
+        reader.readFile(PATH_GENOMA);
+        reader.readFile(PATH_COOR);
 
     	Tools tools = new Tools();
     	StructBuilder structBuilder = new StructBuilder();
