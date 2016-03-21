030bd6de5d7765c5eadcd44b446aebef0e615c34
LeonardoAndriotti <leonardoandriotte@hotmail.com>
2016-03-21 18:18:12 -0300

    commit léo

--- src/main/java/org/alvorada/cc/ep5/lists/LinkedList.java
+++ src/main/java/org/alvorada/cc/ep5/lists/LinkedList.java
@@ -1,12 +1,34 @@
 package org.alvorada.cc.ep5.lists;
 
+import org.alvorada.cc.ep5.Main;
 import org.alvorada.cc.ep5.domain.Node;
 
 public class LinkedList {
 
-	public Node[] changeSubsequence(int[] coordenadasDasSubsequencias, Node[] nodes) {
+	public String [] changeSubsequence(int[] coordenadasDasSubsequencias, String string) {
 		// TODO inverter subsequencias de bases
-		return null;
+		int contador =0;
+		String [] array = string.split("");
+		for (int i = 0; i < coordenadasDasSubsequencias.length -1; i++) {
+			
+		String aux = array[contador];
+    	array[contador]= array[contador++];
+    	array[contador++]= aux;
+    	contador ++;
+    	System.out.println(contador );
+    	
+		}
+    	
+		for (int i = 0; i < array.length; i++) {
+
+			//Main.root.addInthefirst(array[i]);
+		}
+		return array;
+	}
+	public static void main(String[] args) {
+		LinkedList a = new LinkedList();
+		int[]array={1,4,2,3};
+		a.changeSubsequence(array, "popeie");
 	}
 
 }
