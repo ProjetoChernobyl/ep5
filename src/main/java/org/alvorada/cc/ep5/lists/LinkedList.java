40eb19ea42989bc80a157d7bc70128d62c5265e0
LeonardoAndriotti <leonardoandriotte@hotmail.com>
2016-03-22 11:14:10 -0300

    update LinkedList 22-03-2016

--- src/main/java/org/alvorada/cc/ep5/lists/LinkedList.java
+++ src/main/java/org/alvorada/cc/ep5/lists/LinkedList.java
@@ -1,34 +1,35 @@
 package org.alvorada.cc.ep5.lists;
 
+import java.util.Arrays;
+
 import org.alvorada.cc.ep5.Main;
 import org.alvorada.cc.ep5.domain.Node;
 
 public class LinkedList {
 
-	public String [] changeSubsequence(int[] coordenadasDasSubsequencias, String string) {
+	public String[] changeSubsequence(int[] coordenadasDasSubsequencias, String string) {
 		// TODO inverter subsequencias de bases
-		int contador =0;
-		String [] array = string.split("");
-		for (int i = 0; i < coordenadasDasSubsequencias.length -1; i++) {
+		String[] arrayString = string.split("");
+		int contador = 0;
+		for (int i = 0;i< coordenadasDasSubsequencias.length-1;i++) {
+			String aux = arrayString[coordenadasDasSubsequencias[i]];
+			arrayString[coordenadasDasSubsequencias[i]] = arrayString[coordenadasDasSubsequencias[i + 1]];
+			arrayString[coordenadasDasSubsequencias[i + 1]] = aux;
+			i++;
+			System.out.println(Arrays.toString(arrayString));
 			
-		String aux = array[contador];
-    	array[contador]= array[contador++];
-    	array[contador++]= aux;
-    	contador ++;
-    	System.out.println(contador );
-    	
+			System.out.println(i);
 		}
-    	
-		for (int i = 0; i < array.length; i++) {
 
-			//Main.root.addInthefirst(array[i]);
-		}
-		return array;
+		// Main.root.addInthefirst(array[i]);
+		System.out.println(Arrays.toString(arrayString));
+		return arrayString;
 	}
+
 	public static void main(String[] args) {
 		LinkedList a = new LinkedList();
-		int[]array={1,4,2,3};
-		a.changeSubsequence(array, "popeie");
+		int[] array = {1,4,2,3,5,7,8,9};
+		a.changeSubsequence(array, "ABCDEFG48");
 	}
 
 }
