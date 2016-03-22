cf032b5160d5e79f4e9eb2fd907f55dd31915184
Ricardo Luiz Silveira <ricardo.silveira@gmail.com>
2016-03-22 12:03:49 -0300

    Update LinkedListTest.java

--- src/test/java/org/alvorada/cc/ep5/lists/LinkedListTest.java
+++ src/test/java/org/alvorada/cc/ep5/lists/LinkedListTest.java
@@ -1,58 +1,21 @@
 package org.alvorada.cc.ep5.lists;
 
-import org.alvorada.cc.ep5.domain.Node;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class LinkedListTest {
 	
 	LinkedList classUnderTest = new LinkedList();
-	Node rootNodeTest = new Node('R');
-	
-	public Node[] initNodes(){
-		Node[] nodes = new Node[10];
-		nodes[0] = new Node('T');
-		nodes[1] = new Node('T');
-		nodes[2] = new Node('A');
-		nodes[3] = new Node('C');
-		nodes[4] = new Node('T');
-		nodes[5] = new Node('G');
-		nodes[6] = new Node('C');
-		nodes[7] = new Node('A');
-		nodes[8] = new Node('T');
-		nodes[9] = new Node('G');
-		
-		rootNodeTest.setNext(nodes[0]);
-		nodes[0].setNext(nodes[1]);
-		nodes[1].setNext(nodes[2]);
-		nodes[2].setNext(nodes[3]);
-		nodes[3].setNext(nodes[4]);
-		nodes[4].setNext(nodes[5]);
-		nodes[5].setNext(nodes[6]);
-		nodes[6].setNext(nodes[7]);
-		nodes[7].setNext(nodes[8]);
-		nodes[8].setNext(nodes[9]);		
-		
-		return nodes;
-	}
 	
 	
+	@Test
 	public void testChangeSubsequence() {
 		int[] inverte = {3, 6};
-		Node[] nodes = initNodes();
 		
-		String actual1 = "";
-		String expected1 = "TTGTCACATG";
+		String actual = classUnderTest.changeSubsequence(inverte, "TTACTGCATG");
+		String expected = "TTGTCACATG";
 		
-		for (int i = 0; i < nodes.length; i++) {
-			actual1.concat(String.valueOf(nodes[i]));
-		}
-		Assert.assertEquals(expected1, actual1);
-		
-		// TODO teste não finalizado
-		Node[] actuals = classUnderTest.changeSubsequence(inverte, nodes);
-		Node[] expecteds = nodes;
-		Assert.assertArrayEquals(expecteds, actuals);
+		Assert.assertEquals(expected, actual);
 	}
 
 }
