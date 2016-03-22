82bfa818a2a0e2a08de205a2832e271382ba59cb
Ricardo Luiz Silveira <ricardo.silveira@gmail.com>
2016-03-22 12:04:45 -0300

    Update LinkedList.java

--- src/main/java/org/alvorada/cc/ep5/lists/LinkedList.java
+++ src/main/java/org/alvorada/cc/ep5/lists/LinkedList.java
@@ -2,34 +2,27 @@ package org.alvorada.cc.ep5.lists;
 
 import java.util.Arrays;
 
-import org.alvorada.cc.ep5.Main;
-import org.alvorada.cc.ep5.domain.Node;
-
 public class LinkedList {
 
-	public String[] changeSubsequence(int[] coordenadasDasSubsequencias, String string) {
+	public String changeSubsequence(int[] coordenadasDasSubsequencias, String string) {
 		// TODO inverter subsequencias de bases
 		String[] arrayString = string.split("");
 		int contador = 0;
-		for (int i = 0;i< coordenadasDasSubsequencias.length-1;i++) {
+		for (int i = 0; i < coordenadasDasSubsequencias.length - 1; i++) {
 			String aux = arrayString[coordenadasDasSubsequencias[i]];
 			arrayString[coordenadasDasSubsequencias[i]] = arrayString[coordenadasDasSubsequencias[i + 1]];
 			arrayString[coordenadasDasSubsequencias[i + 1]] = aux;
 			i++;
 			System.out.println(Arrays.toString(arrayString));
-			
+
 			System.out.println(i);
 		}
+		StringBuilder builder = new StringBuilder("");
+		for (int i = 0; i < arrayString.length; i++) {
+			builder.append(arrayString[i]);
+		}
 
-		// Main.root.addInthefirst(array[i]);
-		System.out.println(Arrays.toString(arrayString));
-		return arrayString;
-	}
-
-	public static void main(String[] args) {
-		LinkedList a = new LinkedList();
-		int[] array = {1,4,2,3,5,7,8,9};
-		a.changeSubsequence(array, "ABCDEFG48");
+		return builder.toString();
 	}
 
 }
